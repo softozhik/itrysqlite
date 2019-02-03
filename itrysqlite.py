a239e6709d2d310783dfb58f9110ec09858487b4
softozhik <softozhik@live.ru>
2019-02-03 18:51:12 +0300

    пока н7е очень получается вставлять данные в нужную ячейку :-(

--- itrysqlite.py
+++ itrysqlite.py
@@ -2,14 +2,25 @@
 
 import sqlite3
 
-def insert_data(data):
+def insert_Inventory(inv):
+    cursor=connect.cursor()
+    comp=(inv, None, None, None)
     try:
-        cursor.execute("""insert into computers values (data);""")
+        cursor.execute("""insert into computers values (?, ?, ?, ?)""", comp)
         connect.commit()
     except sqlite3.Error as e:
         print (e)
+    cursor.close()
+
 
 connect=sqlite3.connect("mydb.sqlite")
-cursor=connect.cursor()
 
-cursor.close()
+
+inv = input("Inventory (числовой, уникальный): ")
+#user= input("User: ")
+#os = input("OS: ")
+#compname = input("CompName: ")
+#comp=(inv, user, os, compname)
+#print(comp)
+insert_Inventory(inv)
+
